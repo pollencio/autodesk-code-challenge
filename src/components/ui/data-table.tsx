cdb32640b19cd7a5dfe080369b6051a07de4bc0c
Yeliana Torres <yeliana.torres@globant.com>
2025-04-04 17:30:56 -0500

    create data table component and hook

---
+++ src/components/ui/data-table.tsx
@@ -0,0 +1,157 @@
+"use client";
+
+import { useIsMobile } from "@/hooks/use-mobile";
+import { cn } from "@/lib/utils";
+import {
+  type Column,
+  flexRender,
+  type Table as TTable,
+} from "@tanstack/react-table";
+import { ArrowDownUp, ArrowUpZA } from "lucide-react";
+import { ReactNode, useEffect } from "react";
+import { Button } from "./button";
+import { Skeleton } from "./skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./table";
+
+interface DataTableProps<TData> {
+  table: TTable<TData> & { onRowClick?: (row: TData) => void };
+}
+
+export function DataTable<TData>({ table }: DataTableProps<TData>) {
+  const isMobile = useIsMobile();
+  const tableRows = table.getRowModel().rows;
+  const { pageIndex, pageSize } = table.getState().pagination;
+
+  // Hide columns on mobile
+  useEffect(() => {
+    if (isMobile) {
+      table.getAllColumns().map((column) => {
+        if (column.getCanHide()) {
+          column.toggleVisibility();
+        }
+      });
+    }
+  }, [isMobile, table]);
+
+  // Show loading skeleton
+  // TODO: Add  ´|| data === undefined´ to the conditional
+  if (isMobile === undefined)
+    return (
+      <div className="flex flex-1 flex-col grow gap-2 h-full items-end">
+        <Skeleton className="size-full" />
+        <Skeleton className="h-9 w-96 flex" />
+      </div>
+    );
+
+  return (
+    <div className="space-y-4">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {tableRows?.length ? (
+            tableRows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+                onClick={() => table.onRowClick?.(row.original)}
+                className={cn({ "cursor-pointer": !!table.onRowClick })}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell
+                colSpan={table.getAllColumns().length}
+                className="h-24 text-center"
+              >
+                No hay resultados.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      {!!tableRows?.length && pageSize < table.getRowCount() && (
+        <div className="flex items-center justify-between">
+          <div className="text-sm text-muted-foreground">
+            {!isMobile && "Mostrando del"} {pageIndex * pageSize + 1} al{" "}
+            {pageSize > table.getRowCount()
+              ? table.getRowCount()
+              : (pageIndex + 1) * pageSize}{" "}
+            de {table.getRowCount()} resultados.
+          </div>
+          <div className="flex items-center space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Anterior
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Siguiente
+            </Button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+interface SortableTableHeaderProps<TData> {
+  column: Column<TData, unknown>;
+  children: ReactNode;
+}
+
+export function SortableTableHeader<TData>({
+  column,
+  children,
+}: SortableTableHeaderProps<TData>) {
+  const isSorted = column.getIsSorted();
+
+  return (
+    <Button
+      variant="ghost"
+      onClick={() => column.toggleSorting()}
+      className="[&_svg]:size-3.5"
+    >
+      {children}
+      {!isSorted && <ArrowDownUp />}
+      {isSorted && (isSorted === "asc" ? <ArrowUpZA /> : <ArrowUpZA />)}
+    </Button>
+  );
+}
---
+++ src/hooks/use-data-table.ts
@@ -0,0 +1,56 @@
+"use client";
+
+import {
+  ColumnFiltersState,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  TableOptions,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+import { useState } from "react";
+
+type UseDataTableProps<TData> = Omit<
+  TableOptions<TData>,
+  | "getCoreRowModel"
+  | "getPaginationRowModel"
+  | "getSortedRowModel"
+  | "getFilteredRowModel"
+> & { onRowClick?: (row: TData) => void };
+
+export function useDataTable<TData>(props: UseDataTableProps<TData>) {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  const { data, columns, state, initialState, onRowClick, ...options } = props;
+
+  const table = useReactTable({
+    data: data || [],
+    columns: columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      ...state,
+    },
+    initialState: {
+      pagination: { pageSize: 15 },
+      ...initialState,
+    },
+
+    ...options,
+  });
+
+  return { ...table, onRowClick };
+}
