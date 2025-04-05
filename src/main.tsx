032783d2850c2bbfcf20f28a8a103f6dac10eba4
Yeliana Torres <yeliana.torres@globant.com>
2025-04-05 14:19:59 -0500

    create toaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
 import { Providers } from "./components/providers.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
@@ -9,5 +10,6 @@ createRoot(document.getElementById("root")!).render(
     <Providers>
       <App />
     </Providers>
+    <Toaster />
   </StrictMode>,
 );
